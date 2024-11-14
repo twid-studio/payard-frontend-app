1ef6cda996ed91ed096d00c451a430fda825d40d
ValeriiViachalo <valeraviachalo@gmail.com>
2024-11-14 13:25:41 +0200

    google search console

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="html" lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="XhHJqlKd8WAOLMijvniQgfDcImT2HG6c8tzfjE64iZo"
+        />
+      </Head>
       <body className="body">
         <Main />
         <NextScript />
