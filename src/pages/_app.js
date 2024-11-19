fe5b76fb4d2ec1c72d1809f069f9ee5f5641bf9c
ValeriiViachalo <valeraviachalo@gmail.com>
2024-11-19 13:41:07 +0200

    Google analitycs implemented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,38 @@
 import "@/styles/reset.scss";
 import { CookiePopUp } from "@/utils/CookiePopUp/CookiePopUp";
 import { AnimatePresence } from "framer-motion";
+import Script from "next/script";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryC = new QueryClient();
 
 export default function App({ Component, pageProps, router }) {
   return (
-    <div className="main">
-      <QueryClientProvider client={queryC}>
-        <AnimatePresence mode="wait">
-          <Component key={router.route} {...pageProps} />
-          <CookiePopUp />
-        </AnimatePresence>
-      </QueryClientProvider>
-    </div>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-XG58B9L0S1`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XG58B9L0S1');
+          `,
+        }}
+      />
+      <div className="main">
+        <QueryClientProvider client={queryC}>
+          <AnimatePresence mode="wait">
+            <Component key={router.route} {...pageProps} />
+            <CookiePopUp />
+          </AnimatePresence>
+        </QueryClientProvider>
+      </div>
+    </>
   );
 }
