de76ddffa5ba2ec0b1a7a9c068e26d600ca8037e
ValeriiViachalo <valeraviachalo@gmail.com>
2024-11-19 13:50:27 +0200

    Google analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/reset.scss";
 import { CookiePopUp } from "@/utils/CookiePopUp/CookiePopUp";
 import { AnimatePresence } from "framer-motion";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryC = new QueryClient();
@@ -9,22 +9,7 @@ const queryC = new QueryClient();
 export default function App({ Component, pageProps, router }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-XG58B9L0S1`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XG58B9L0S1');
-          `,
-        }}
-      />
+      <GoogleAnalytics gaId="G-XG58B9L0S1" />{" "}
       <div className="main">
         <QueryClientProvider client={queryC}>
           <AnimatePresence mode="wait">
