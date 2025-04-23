9a220b60eeb4b65b8c7e137237d5426bb544953e
ValeriiViachalo <valeraviachalo@gmail.com>
2025-04-23 14:17:28 +0300

    Refactor PageHead component to remove unused Script import and streamline meta tags structure

--- src/utils/PageHead/PageHead.jsx
+++ src/utils/PageHead/PageHead.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Script from "next/script";
 
 export const PageHead = ({ data }) => {
   const pageTitle = data?.pageTitle;
@@ -9,160 +8,142 @@ export const PageHead = ({ data }) => {
   const metaKeywords = data?.keywords;
 
   return (
-      <>
-        <Head>
-          {/* Basic Meta Tags */}
-          {metaTitle && <title>{pageTitle}</title>}
-          {metaDescription && <meta name="description" content={metaDescription} />}
-          {metaKeywords && <meta name="keywords" content={metaKeywords} />}
+    <Head>
+      {/* Basic Meta Tags */}
+      {metaTitle && <title>{pageTitle}</title>}
+      {metaDescription && <meta name="description" content={metaDescription} />}
+      {metaKeywords && <meta name="keywords" content={metaKeywords} />}
 
-          {/* Open Graph Meta Tags */}
-          {metaTitle && <meta property="og:title" content={metaTitle} />}
-          {metaDescription && (
-            <meta property="og:description" content={metaDescription} />
-          )}
-          {metaImage && <meta property="og:image" content={metaImage} />}
-          <meta property="og:locale" content={data?.locale || "en"} />
+      {/* Open Graph Meta Tags */}
+      {metaTitle && <meta property="og:title" content={metaTitle} />}
+      {metaDescription && (
+        <meta property="og:description" content={metaDescription} />
+      )}
+      {metaImage && <meta property="og:image" content={metaImage} />}
+      <meta property="og:locale" content={data?.locale || "en"} />
 
-          {/* Twitter Card Meta Tags */}
-          <meta name="twitter:card" content="summary_large_image" />
-          {metaTitle && <meta name="twitter:title" content={metaTitle} />}
-          {metaDescription && (
-            <meta name="twitter:description" content={metaDescription} />
-          )}
-          {metaImage && <meta name="twitter:image" content={metaImage} />}
-          {data?.twitterUsername && (
-            <meta name="twitter:creator" content={data.twitterUsername} />
-          )}
+      {/* Twitter Card Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      {metaTitle && <meta name="twitter:title" content={metaTitle} />}
+      {metaDescription && (
+        <meta name="twitter:description" content={metaDescription} />
+      )}
+      {metaImage && <meta name="twitter:image" content={metaImage} />}
+      {data?.twitterUsername && (
+        <meta name="twitter:creator" content={data.twitterUsername} />
+      )}
 
-          {/* Image Meta Tags */}
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          {metaTitle && <meta property="og:image:alt" content={metaTitle} />}
+      {/* Image Meta Tags */}
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      {metaTitle && <meta property="og:image:alt" content={metaTitle} />}
 
-          {/* Viewport */}
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
-          />
+      {/* Viewport */}
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
 
-          {/* Character Set */}
-          <meta charSet="utf-8" />
+      {/* Character Set */}
+      <meta charSet="utf-8" />
 
-          {/* X-UA-Compatible */}
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      {/* X-UA-Compatible */}
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-          {/* #region */}
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="57x57"
-            href="/favicon/apple-touch-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="114x114"
-            href="/favicon/apple-touch-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="72x72"
-            href="/favicon/apple-touch-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="144x144"
-            href="/favicon/apple-touch-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="60x60"
-            href="/favicon/apple-touch-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="120x120"
-            href="/favicon/apple-touch-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="76x76"
-            href="/favicon/apple-touch-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="152x152"
-            href="/favicon/apple-touch-icon-152x152.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicon/favicon-196x196.png"
-            sizes="196x196"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicon/favicon-96x96.png"
-            sizes="96x96"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicon/favicon-32x32.png"
-            sizes="32x32"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicon/favicon-16x16.png"
-            sizes="16x16"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicon/favicon-128.png"
-            sizes="128x128"
-          />
-          <meta name="application-name" content="&nbsp;" />
-          <meta name="msapplication-TileColor" content="#FFFFFF" />
-          <meta
-            name="msapplication-TileImage"
-            content="/favicon/mstile-144x144.png"
-          />
-          <meta
-            name="msapplication-square70x70logo"
-            content="/favicon/mstile-70x70.png"
-          />
-          <meta
-            name="msapplication-square150x150logo"
-            content="mstile-150x150.png"
-          />
-          <meta
-            name="msapplication-wide310x150logo"
-            content="/favicon/mstile-310x150.png"
-          />
-          <meta
-            name="msapplication-square310x310logo"
-            content="mstile-310x310.png"
-          />
-          {/* #endregion */}
-        </Head>
-
-        {/* Google Tag Manager */}
-        <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-                __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WTTLBK9Z');
-              `,
-            }}
-        />
-  </>
+      {/* #region */}
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="57x57"
+        href="/favicon/apple-touch-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="114x114"
+        href="/favicon/apple-touch-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="72x72"
+        href="/favicon/apple-touch-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="144x144"
+        href="/favicon/apple-touch-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="60x60"
+        href="/favicon/apple-touch-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="120x120"
+        href="/favicon/apple-touch-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="76x76"
+        href="/favicon/apple-touch-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="152x152"
+        href="/favicon/apple-touch-icon-152x152.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon/favicon-196x196.png"
+        sizes="196x196"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon/favicon-128.png"
+        sizes="128x128"
+      />
+      <meta name="application-name" content="&nbsp;" />
+      <meta name="msapplication-TileColor" content="#FFFFFF" />
+      <meta
+        name="msapplication-TileImage"
+        content="/favicon/mstile-144x144.png"
+      />
+      <meta
+        name="msapplication-square70x70logo"
+        content="/favicon/mstile-70x70.png"
+      />
+      <meta
+        name="msapplication-square150x150logo"
+        content="mstile-150x150.png"
+      />
+      <meta
+        name="msapplication-wide310x150logo"
+        content="/favicon/mstile-310x150.png"
+      />
+      <meta
+        name="msapplication-square310x310logo"
+        content="mstile-310x310.png"
+      />
+      {/* #endregion */}
+    </Head>
   );
 };
-
